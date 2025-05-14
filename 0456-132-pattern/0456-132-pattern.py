c75e77faf72e99bbc83131fb94ea71458effd8a6
Daksh Viradiya <85271867+DakshVR@users.noreply.github.com>
2025-05-14 13:23:51 -0400

    Time: 40 ms (75.54%), Space: 35.5 MB (7.56%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,14 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        third = float('-inf')
+
+        # Traverse from right to left
+        for num in reversed(nums):
+            if num < third:
+                return True
+            while stack and num > stack[-1]:
+                third = stack.pop()
+            stack.append(num)
+
+        return False
